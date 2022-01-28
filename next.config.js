80e262e5ab99a6c297dfe5b73c684988f2810fdd
Carmelo Scandaliato <8927157+cascandaliato@users.noreply.github.com>
2022-01-28 07:13:37 +0000

    Make redirects conditional

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/get/:file/:password',
-        destination: `${process.env.SECRETS_API_PROJECT_URL}/api/get?password=:password&file=:file`,
+        source: `/get/:file/:password(${process.env.SECRETS_API_PASSWORD})`,
+        destination: `${process.env.SECRETS_API_URL}/api/get?password=:password&file=:file`,
         permanent: false,
       },
       {
@@ -13,7 +13,14 @@ module.exports = {
         destination: 'https://github.com/cascandaliato/:repo/commit/:commit',
         permanent: false,
       },
-
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: `/get2/:file/:password(${process.env.SECRETS_API_PASSWORD})`,
+        destination: `${process.env.SECRETS_API_URL}/api/get?password=:password&file=:file`,
+      },
     ]
   },
 }
